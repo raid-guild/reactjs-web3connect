21f3a4330bd75194665b56d2d7cfdd73819a524a
Dekan <dekanbro@gmail.com>
2020-03-02 13:28:10 -0700

    example with weth contract

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React, { useContext } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import { Web3SignIn } from './components/account/Web3SignIn';
 import { CurrentUserContext } from './contexts/Store';
+import { DepositForm } from './components/DepositForm';
+import { WithdrawForm } from './components/WithdrawForm';
+import { TokenInfo } from './components/TokenInfo';
 
 
 function App() {
   const [currentUser, setCurrentUser] = useContext(CurrentUserContext);
-  console.log('currentUser', currentUser);
 
   return (
     <div className="App">
       <Container>
         <Row>
-          <Col>1 of 2</Col>
+          <Col>Weth Wrapper</Col>
           <Col>
             {currentUser && currentUser.username ? (<p>{currentUser.username}</p>) : (<Web3SignIn setCurrentUser={setCurrentUser} />)}
           </Col>
         </Row>
         <Row>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
+          <Col>{currentUser && currentUser.username ? (<DepositForm />) : (<p>Must sign in first</p>)}</Col>
+          <Col><TokenInfo /></Col>
+          <Col>{currentUser && currentUser.username ? (<WithdrawForm />) : (<p>Must sign in first</p>)}</Col>
         </Row>
       </Container>
     </div>
--- src/contexts/Store.js
+++ src/contexts/Store.js
@@ -7,13 +7,17 @@ import {
 } from '../utils/Auth';
 import { getChainData } from '../utils/Chains';
 
+import WethAbi from '../contracts/wethAbi.json';
+
 export const LoaderContext = createContext(false);
 export const Web3ConnectContext = createContext();
 export const CurrentUserContext = createContext();
+export const ContractContext = createContext();
 
 const Store = ({ children }) => {
 
     const [currentUser, setCurrentUser] = useState();
+    const [contract, setContract] = useState();
     const [loading, setLoading] = useState(false);
     const [web3Connect, setWeb3Connect] = useState(
         new Web3Connect.Core({
@@ -24,16 +28,14 @@ const Store = ({ children }) => {
     );
 
     useEffect(() => {
-
-        const initCurrentUser = async () => {
-            let user;
+        const onLoad = async () => {
             try {
                 const w3c = await w3connect(
                     web3Connect,
                 );
                 const [account] = await w3c.web3.eth.getAccounts();
                 setWeb3Connect(w3c);
-                user = createWeb3User(account);
+                const user = createWeb3User(account);
                 setCurrentUser(user);
             } catch (e) {
                 console.error(
@@ -42,16 +44,38 @@ const Store = ({ children }) => {
             }
         };
         if (web3Connect.cachedProvider) {
-            initCurrentUser();
+            onLoad();
         }
-    }, [web3Connect]);
+        // eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
+        const initContract = async () => {
+            try {
+                const contract = new web3Connect.web3.eth.Contract(WethAbi, process.env.REACT_APP_CONTRACT_ADDR);
+                setContract(contract)
+            } catch (e) {
+                console.error(
+                    `Could not init contract`,
+                );
+            }
+        };
+        if (web3Connect.web3) {
+            initContract();
+        }
+        // eslint-disable-next-line
+    }, [web3Connect.web3]);
+
+
     return (
         <LoaderContext.Provider value={[loading, setLoading]}>
             <Web3ConnectContext.Provider value={[web3Connect, setWeb3Connect]}>
                 <CurrentUserContext.Provider
                     value={[currentUser, setCurrentUser]}
                 >
-                    {children}
+                    <ContractContext.Provider value={[contract, setContract]}>
+                        {children}
+                    </ContractContext.Provider>
                 </CurrentUserContext.Provider>
             </Web3ConnectContext.Provider>
         </LoaderContext.Provider>
